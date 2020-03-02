1161d7ff1163759db3062966c21ef4c682b840bd
sashacmc <Alexander@Bushnev.ru>
2020-03-03 00:38:59 +0100

    Fix facediff

--- facediff.py
+++ facediff.py
@@ -13,27 +13,45 @@ encoder = faceencoder.FaceEncoder('VGG-Face', 'cosine')
 
 
 def get_face(fname):
+    """
     try:
         encoding = pickle.loads(
             piexif.load(fname)["0th"][piexif.ImageIFD.ImageDescription])
         print('Use cached: ' + fname)
-        return encoding
+        return encoding['encoding']
     except Exception:
         pass
-
-    image = tools.read_image(fname, 1000)
+"""
+    try:
+        image = tools.read_image(fname, 1000)
+    except Exception:
+        print(f'image {fname} reading failed')
+        return None
     boxes = face_recognition.face_locations(image, model='cnn')
+    if len(boxes) != 1:
+        print(f'Image contains {len(boxes)} faces')
+        return None
     return encoder.encode(image, boxes)[0]
 
 
 def main():
     encoding_base = get_face(sys.argv[1])
+    if encoding_base is None:
+        return
     fnames = sys.argv[2:]
     encodings = [get_face(fname) for fname in fnames]
 
-    distances = encoder.distance(encodings, encoding_base)
+    encodings_filtered = []
+    fnames_filtered = []
+    for e, f in zip(encodings, fnames):
+        if e is not None:
+            encodings_filtered.append(e)
+            fnames_filtered.append(f)
 
-    dist_name = [(distances[i], fname) for i, fname in enumerate(fnames)]
+    distances = encoder.distance(encodings_filtered, encoding_base)
+
+    dist_name = [(distances[i], fname)
+                 for i, fname in enumerate(fnames_filtered)]
     dist_name.sort()
 
     for d, n in dist_name:
